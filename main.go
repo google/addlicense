d5a1286c05135ee4a475ed6edecb6d0011b6c643
YangjingZhou <ZhouYangjing314@gmail.com>
2020-03-20 09:53:37 +0800

    feat:add filetype vue

--- main.go
+++ main.go
@@ -219,7 +219,7 @@ func licenseHeader(path string, tmpl *template.Template, data *copyrightData) ([
 		lic, err = prefix(tmpl, data, "", "% ", "")
 	case ".hs", ".sql":
 		lic, err = prefix(tmpl, data, "", "-- ", "")
-	case ".html", ".xml":
+	case ".html", ".xml", ".vue":
 		lic, err = prefix(tmpl, data, "<!--", " ", "-->")
 	case ".php":
 		lic, err = prefix(tmpl, data, "", "// ", "")
