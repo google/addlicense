9e09af591bf3e63f28674bd87b9fc1cdfb5b2def
Will Norris <will@willnorris.com>
2021-08-10 09:50:06 -0700

    feat: add support for cmake files
    Co-authored-by: Sergii Baitala <sbaitala@gmail.com>

--- main.go
+++ main.go
@@ -306,6 +306,11 @@ func licenseHeader(path string, tmpl *template.Template, data licenseData) ([]by
 		lic, err = executeTemplate(tmpl, data, "", "// ", "")
 	case ".ml", ".mli", ".mll", ".mly":
 		lic, err = executeTemplate(tmpl, data, "(**", "   ", "*)")
+	default:
+		// handle various cmake files
+		if base == "cmakelists.txt" || strings.HasSuffix(base, ".cmake.in") || strings.HasSuffix(base, ".cmake") {
+			lic, err = executeTemplate(tmpl, data, "", "# ", "")
+		}
 	}
 	return lic, err
 }
