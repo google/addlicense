f387698da4a6e79775dfe502f5c783f0dbb8af5e
Tim 'mithro' Ansell <tansell@google.com>
2022-07-14 17:29:39 -0700

    Adding support for spice files.
    Signed-off-by: Tim 'mithro' Ansell <tansell@google.com>

--- main.go
+++ main.go
@@ -307,6 +307,8 @@ func licenseHeader(path string, tmpl *template.Template, data licenseData) ([]by
 		lic, err = executeTemplate(tmpl, data, "", "// ", "")
 	case ".ml", ".mli", ".mll", ".mly":
 		lic, err = executeTemplate(tmpl, data, "(**", "   ", "*)")
+	case ".hspice", ".spice", ".cdl":
+		lic, err = executeTemplate(tmpl, data, "", "* ", "")
 	default:
 		// handle various cmake files
 		if base == "cmakelists.txt" || strings.HasSuffix(base, ".cmake.in") || strings.HasSuffix(base, ".cmake") {
