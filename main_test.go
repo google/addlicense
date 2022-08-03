a4887357f3d392ed86dbe0fbc5b0435c08625441
Caleb Albers <calbers@hashicorp.com>
2022-08-03 16:01:58 -0700

    Add .bash and .zsh extensions
    Although less common than `.sh`, `.bash` and `.zsh` extensions do float around with some frequency.

--- main_test.go
+++ main_test.go
@@ -316,7 +316,7 @@ func TestLicenseHeader(t *testing.T) {
 			"// HYS\n\n",
 		},
 		{
-			[]string{"f.py", "f.sh", "f.yaml", "f.yml", "f.dockerfile", "dockerfile", "f.rb", "gemfile", "f.tcl", "f.bzl", "f.pl", "f.pp"},
+			[]string{"f.py", "f.sh", "f.bash", "f.zsh", "f.yaml", "f.yml", "f.dockerfile", "dockerfile", "f.rb", "gemfile", "f.tcl", "f.bzl", "f.pl", "f.pp"},
 			"# HYS\n\n",
 		},
 		{
