9d0b26789cfc7d0c9a614d41700309ccbdb38409
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-06-04 20:20:55 +0800

    Add a separator after the license header if the template has no bottom

--- tmpl_test.go
+++ tmpl_test.go
@@ -204,6 +204,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+================================================================================
 
 `,
 		},
@@ -242,6 +243,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+================================================================================
 
 `,
 		},
